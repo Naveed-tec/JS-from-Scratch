fd0e952ad7793cdba138e14a9d3ce7a824ea5e5d
Muhammad Naveed <mu42ghal.33@gmail.com>
2025-04-12 17:18:34 +0000

    challenge

---
+++ Operators/challenge3.js
@@ -0,0 +1,16 @@
+let bill = 400;
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill is ${bill} , The tip is ${tip} and the total is ${bill + tip}`)
+
+
+/*bill = 400;
+bill <= 50 || bill >= 300 ? console.log(bill, tip = (20/100)*bill): bill
+total = tip + bill
+console.log(total)
+
+
+bill = 40;
+bill <= 50 || bill >= 300 ? tip = (20/100)*bill: bill
+console.log(tip)
+total = tip + bill
+console.log(total)*/
